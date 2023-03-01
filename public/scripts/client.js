a5528be70dbcc8ba5d8af54a511b5b28631f21e9
Kate <115913025+bootonk@users.noreply.github.com>
2023-03-01 12:25:34 -0800

    Load tweet database via ajax and remove hardcoded tests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" 
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweetArr) {
   for (const tweet of tweetArr) {
     const buildNewTweet = createTweetElement(tweet);
@@ -69,11 +44,8 @@ const createTweetElement = function(tweetObj) {
 };
 
 $(document).ready(function() {
-  // process tweets array & format each into HTML element to append
-  renderTweets(data);
-
   // override standard form submission with ajax version
-  $( "#postTweet" ).submit(function( event ) {
+  $( "#post-tweet" ).submit(function( event ) {
     event.preventDefault();
 
     const serializedURL = $( this ).serialize();
@@ -81,4 +53,17 @@ $(document).ready(function() {
     
     $( "#tweet-text" ).val("");
   });
+
+  // load tweets from external server
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (responseData) => { renderTweets(responseData) },
+      error: (err) => { console.log("ah ah ahhhh", err.message) }
+    })
+  };
+
+  loadTweets();
+
 });
