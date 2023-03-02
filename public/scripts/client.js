1a7965c453561a6ddbacf55b030e89925270e732
Kate <115913025+bootonk@users.noreply.github.com>
2023-03-01 17:58:04 -0800

    Refactor to improve page load consistency

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,47 +3,59 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const renderTweets = function(tweetArr) {
-  for (const tweet of tweetArr) {
-    const buildNewTweet = createTweetElement(tweet);
-    $('#tweets-container').prepend(buildNewTweet);
-  }
-};
-
-const createTweetElement = function(tweetObj) {
-  const newArticle = $("<article>").addClass("tweet");
-  
-  const newHeader = `
-        <header>
-          <div class="left-header">
-            <img src="${tweetObj.user.avatars}">
-            <p>${tweetObj.user.name}</p>
-          </div>
-          <div class="right-header">
-            <p>${tweetObj.user.handle}</p>
-          </div>
-        </header>`;
-
-  const newBody = $("<p>").addClass("content").text(tweetObj.content.text);
-  
-  const newFooter = `
-        <footer>
-          <p>${timeago.format(tweetObj.created_at)}</p>
-          <div class="footer-icons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>`
-
-  newArticle.append(newHeader);
-  newArticle.append(newBody);
-  newArticle.append(newFooter);
-
-  return newArticle;
-};
-
 $(document).ready(function() {
+  const createTweetElement = function(tweetObj) {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+  
+    const tweetElement = `
+    <article class="tweet">
+      <header>
+        <div class="left-header">
+          <img src="${tweetObj.user.avatars}">
+          <p>${tweetObj.user.name}</p>
+        </div>
+        <div class="right-header">
+          <p>${tweetObj.user.handle}</p>
+        </div>
+      </header>
+      <p class="content">${escape(tweetObj.content.text)}</p>
+      <footer>
+        <p>${timeago.format(tweetObj.created_at)}</p>
+        <div class="footer-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `
+    return tweetElement;
+  };
+
+  const renderTweets = function(tweetArr) {
+    $( "#tweets-container" ).empty();
+    for (const tweet of tweetArr) {
+      const buildNewTweet = createTweetElement(tweet);
+      $( '#tweets-container' ).prepend(buildNewTweet);
+    }
+  };
+  
+  // load tweets from external server
+  const loadTweets = function() {
+    $.get( "/tweets" )
+    .done(function (tweetData) {
+      renderTweets(tweetData);
+    }).fail(function( jqXHR, textStatus, errorThrown ) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown );
+  });
+  };
+
   // override standard form submission with ajax version
     $( "#post-tweet" ).submit(function( event ) {
       event.preventDefault();
@@ -54,29 +66,19 @@ $(document).ready(function() {
         alert("cannot post tweet, too high")
       } else {
         const serializedURL = $( this ).serialize();
-        $.post( "/tweets", serializedURL);
+        $.post( "/tweets", serializedURL)
+        .done(function( data ) {
+          $( "#tweet-text" ).val("");
+          $( ".counter" ).val(140);
         
-        $( "#tweet-text" ).val("");
-        $( ".counter" ).val(140);
-
-        loadTweets();
+          loadTweets();
+        }).fail(function( jqXHR, textStatus, errorThrown ) {
+          console.log(jqXHR);
+          console.log(textStatus);
+          console.log(errorThrown );
+        });  
       }
-  
     });
 
-  // load tweets from external server
-  const loadTweets = function() {
-    $.ajax({
-      method: "GET",
-      url: "/tweets",
-      success: (responseData) => {
-        $( "#tweets-container" ).empty(); 
-        renderTweets(responseData) 
-      },
-      error: (err) => { console.log("ah ah ahhhh", err.message) }
-    })
-  };
-
   loadTweets();
-
 });
