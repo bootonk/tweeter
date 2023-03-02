1d2994eba335bfa41775013e4e24216a286066d8
Kate <115913025+bootonk@users.noreply.github.com>
2023-03-02 12:07:41 -0800

    Improve styling and include second toggle button

--- public/scripts/composer-form.js
+++ public/scripts/composer-form.js
@@ -2,10 +2,36 @@ $(document).ready(function() {
   // default states
   $( ".new-tweet" ).hide();
   $( ".error " ).hide();
+  $( ".scroll-container" ).hide();
 
   // toggle tweet form with write tweet nav text
   $( ".toggle-form" ).click(function() {
-    $( ".new-tweet" ).toggle( "slow" )
-    $( "#post-tweet textarea").focus();
+    if ( $( ".new-tweet" ).is(":visible")) {
+      $( ".new-tweet" ).toggle( "slow" );
+      $( ".right-nav p" ).text( "Write a new tweet" );
+      $(this).find($(".fa-solid")).removeClass('fa-angles-up').addClass('fa-angles-down');
+    } else {
+      $( ".new-tweet" ).toggle( "slow" );
+      $( "#post-tweet textarea").focus();
+      $( ".right-nav p" ).text( "Hide tweet form" );
+      $(this).find($(".fa-solid")).removeClass('fa-angles-down').addClass('fa-angles-up');
+    }
+  })
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+        $( ".scroll-container" ).show();
+    } else {
+        $( ".scroll-container" ).hide();
+    }
+});
+
+  $( ".scroll-container" ).click(function() {
+    if ( $( ".new-tweet" ).is(":visible")) {
+      $( "#post-tweet textarea").focus();
+    } else {
+      $( ".new-tweet" ).toggle( "slow" );
+      $( "#post-tweet textarea").focus();
+    }
   })
 });
